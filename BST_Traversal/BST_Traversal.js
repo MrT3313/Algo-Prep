e8289f75b6d93ec99bae7e2ef569827bb721c2cf
Mr.T <rturgeon13@gmail.com>
2020-01-09 11:40:34 -0600

    Completed: BST Traversal

--- BST_Traversal/BST_Traversal.js
+++ BST_Traversal/BST_Traversal.js
@@ -23,7 +23,7 @@ console.log(test)
 // FUNCTIONS
 // - 1 - // IN ORDER
 function inOrderTraverse(tree, array) {
-    if (tree !== undefined) {
+    if (tree !== null) {
         inOrderTraverse(tree.left, array)
         array.push(tree.value)
         inOrderTraverse(tree.right, array)
@@ -34,20 +34,22 @@ console.log(inOrderTraverse(test, []))
 
 // - 2 - // PRE ORDER
 function preOrderTraverse(tree, array) {
-    if (tree !== undefined) {
+    if (tree !== null) {
         array.push(tree.value)
         preOrderTraverse(tree.left, array)
         preOrderTraverse(tree.right, array)
     }
     return array
 }
+console.log(preOrderTraverse(test, []))
 
 // - 3 - // POST ORDER
 function postOrderTraverse(tree, array) {
-    if (tree !== undefined) {
+    if (tree !== null) {
         postOrderTraverse(tree.left, array) 
         postOrderTraverse(tree.right, array)
         array.push(tree.value)
     }
     return array
 }
+console.log(postOrderTraverse(test, []))
