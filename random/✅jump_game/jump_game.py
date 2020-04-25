8b25efdd5a39b4dc177c057d7d306df1c20b401a
MrT3313 <turgeon.dev@gmail.com>
2020-04-25 12:11:36 -0500

    Completed Jump Game - leetcode 30day Challenge - day 25

---
+++ random/✅jump_game/jump_game.py
@@ -0,0 +1,13 @@
+def canJump(nums)
+    if not nums:
+        return False
+    i = 0
+    max_index = nums[i]
+    while i < len(nums) and i <= max_index:
+        new_index = nums[i]+i
+        max_index = max(max_index, new_index)
+        i += 1
+    if i == len(nums):
+        return True
+    else:
+        return False
