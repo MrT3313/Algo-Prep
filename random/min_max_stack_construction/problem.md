263af4dfeee2688002cf8e9d93d60b32250d1de9
MrT3313 <turgeon.dev@gmail.com>
2020-06-19 09:52:09 -0400

    Min Max Stack w/ python

---
+++ random/min_max_stack_construction/problem.md
@@ -0,0 +1,7 @@
+Write a `MinMaxStack` class for a Min Max Stack. The class should support:
+
+- Pushing and popping values on and off the stack. 
+- Peeking at the value at the top of the stack.
+- Getting both the minimum and the maximum values in the stack at any given point in time. 
+
+All class methods, when considered independently, should run in constant time and with constant space. 
---
+++ random/min_max_stack_construction/✅ min_max_stack_construction.py
@@ -0,0 +1,28 @@
+# Feel free to add new properties and methods to the class.
+class MinMaxStack:
+    def __init__(self):
+        self.minMaxStack = []
+        self.stack = []
+
+    def peek(self):
+        return self.stack[ len(self.stack) - 1 ]
+
+    def pop(self):
+        self.minMaxStack.pop
+        return self.stack.pop()
+
+    def push(self, number):
+        newMinMax = { "min": number, "max": number }
+        if len(self.minMaxStack):
+            lastMinMax = self.minMaxStack[len(self.minMaxStack) - 1]
+            newMinMax["min"] = min(lastMinMax["min"], number)
+            newMinMax["max"] = min(lastMinMax["max"], number)
+        
+        self.minMaxStack.append(newMinMax)
+        self.stack.append(number)
+
+    def getMin(self):
+        return self.minMaxStack[len(self.minMaxStack) - 1]["min"]
+
+    def getMax(self):
+        return self.minMaxStack[len(self.minMaxStack) -1]["max"]
