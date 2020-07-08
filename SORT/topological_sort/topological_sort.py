e43881e33fa6e4bf24a1a08d4f77b51b41d6af46
MrT3313 <turgeon.dev@gmail.com>
2020-07-08 10:26:36 -0400

    Topological Sort V2 w/ python

--- SORT/topological_sort/topological_sort.py
+++ SORT/topological_sort/topological_sort.py
@@ -1,3 +1,71 @@
+# V1 
+# def topologicalSort(jobs, deps):
+#     jobGraph = createJobGraph(jobs, deps)
+#     return getOrderJobs(jobGraph)
+
+
+# def createJobGraph(jobs, deps):
+#     graph = JobGraph(jobs)
+#     for prereq, job in deps: 
+#         graph.addPrereq(job, prereq)
+#     return graph
+
+# def getOrderJobs(graph):
+#     orderedJobs = []
+#     nodes = graph.nodes
+#     while len(nodes):
+#         node = nodes.pop()
+#         containsCycle = depthFirstTraverse(node, orderedJobs)
+#         if containsCycle:
+#             return []
+#     return orderedJobs
+
+# def depthFirstTraverse(node, orderedJobs):
+#     if node.visited: 
+#         return False 
+#     if node.visiting: 
+#         return True
+#     node.visiting = True
+#     for prereqNode in node.prereqs:
+#         containsCycle = depthFirstTraverse(prereqNode, orderedJobs)
+#         if containsCycle:
+#             return True
+#     node.visited = True
+#     node.visiting = False
+#     orderedJobs.append(node.job)
+#     return False
+        
+# class JobGraph: 
+#     def __init__(self, jobs):
+#         self.nodes = []
+#         self.graph = {}
+
+#         for job in jobs: 
+#             self.addNode(job)
+
+#     def addPrereq(self, job, prereq):
+#         jobNode = self.getNode(job)
+#         prereqNode = self.getNode(prereq)
+#         jobNode.prereqs.append(prereqNode)
+
+#     def addNode(self, job):
+#         self.graph[job] = JobNode(job)
+#         self.nodes.append(self.graph[job])
+
+#     def getNode(self, job):
+#         if job not in self.graph:
+#             self.addNode(job)
+#         return self.graph[job]
+
+# class JobNode: 
+#     def __init__(self, job):
+#         self.job = job
+#         self.prereqs = []
+#         self.visited = False
+#         self.visiting = False
+
+
+# V2 - Track Prerequisites
 def topologicalSort(jobs, deps):
     jobGraph = createJobGraph(jobs, deps)
     return getOrderJobs(jobGraph)
@@ -5,35 +73,27 @@ def topologicalSort(jobs, deps):
 
 def createJobGraph(jobs, deps):
     graph = JobGraph(jobs)
-    for prereq, job in deps: 
-        graph.addPrereq(job, prereq)
+    for job, dep in deps: 
+        graph.addDep(job, dep)
     return graph
 
 def getOrderJobs(graph):
     orderedJobs = []
-    nodes = graph.nodes
-    while len(nodes):
-        node = nodes.pop()
-        containsCycle = depthFirstTraverse(node, orderedJobs)
-        if containsCycle:
-            return []
-    return orderedJobs
-
-def depthFirstTraverse(node, orderedJobs):
-    if node.visited: 
-        return False 
-    if node.visiting: 
-        return True
-    node.visiting = True
-    for prereqNode in node.prereqs:
-        containsCycle = depthFirstTraverse(prereqNode, orderedJobs)
-        if containsCycle:
-            return True
-    node.visited = True
-    node.visiting = False
-    orderedJobs.append(node.job)
-    return False
+    nodesWithNoPrereqs = list(filter(lambda node: node.numOfPrereqs == 0, graph.nodes))
+    while len(nodesWithNoPrereqs):
+        node = nodesWithNoPrereqs.pop()
+        orderedJobs.append(node.job)
+        removeDeps(node, nodesWithNoPrereqs)
+    graphHasEdges = any(node.numOfPrereqs for node in graph.nodes)
+    return [] if graphHasEdges else orderedJobs
         
+def removeDeps(node, nodesWithNoPrereqs):
+    while len(node.deps):
+        dep = node.deps.pop()
+        dep.numOfPrereqs -= 1
+        if dep.numOfPrereqs == 0:
+            nodesWithNoPrereqs.append(dep)
+
 class JobGraph: 
     def __init__(self, jobs):
         self.nodes = []
@@ -42,10 +102,11 @@ class JobGraph:
         for job in jobs: 
             self.addNode(job)
 
-    def addPrereq(self, job, prereq):
+    def addDep(self, job, dep):
         jobNode = self.getNode(job)
-        prereqNode = self.getNode(prereq)
-        jobNode.prereqs.append(prereqNode)
+        depNode = self.getNode(dep)
+        jobNode.deps.append(depNode)
+        depNode.numOfPrereqs += 1
 
     def addNode(self, job):
         self.graph[job] = JobNode(job)
@@ -59,8 +120,5 @@ class JobGraph:
 class JobNode: 
     def __init__(self, job):
         self.job = job
-        self.prereqs = []
-        self.visited = False
-        self.visiting = False
-
-
+        self.deps = []
+        self.numOfPrereqs = 0
